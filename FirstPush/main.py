0fa2943615d9f5bbbdf9ff66d512daf2f06a5f15
Thomashallberg <thomas.hallberg91@outlook.com>
2022-09-05 14:46:44 +0200

    second

---
+++ FirstPush/main.py
@@ -0,0 +1,13 @@
+
+while True:
+print("*** Huvudmeny ***")
+print("1. Skapa konto")
+print("2. Administrera konto")
+print("3. Avsluta")
+inmatning = input("menyval")
+if (input("1")):
+    print("du valde att skapa ett konto")
+elif (input("2")):
+    print("2. administrera")
+elif (input("3"))
+    
