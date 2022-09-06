44ea8fc7b10ba7634f0a7a35ceb205eb45b93fd4
Thomashallberg <thomas.hallberg91@outlook.com>
2022-09-06 19:46:51 +0200

    vilken

---
+++ FirstPush/inlamning/inlamning1.py
@@ -0,0 +1,27 @@
+while True:
+    name = input("vad heter du?")
+    if len(name) < 2 or (name) > 20:
+        print("det kan man väl inte heta?")
+    elif inmatning = input("emailadress"):
+
+    else:
+        break
+    print("hejsan", name)
+    print("Ditt namn börjar på", name[0])
+    print("*** Huvudmeny ***")
+    print("1. Skapa konto")
+    print("2. Administrera konto")
+    print("3. Avsluta")
+    inmatning = input("menyval")
+    Fornamn = "Stefan"
+    Eftername = "Holmberg"
+    Fullname = Fornamn + " " + Eftername
+    text = "I'm a programmer"
+    text2 = 'I am a "programmer" wow'
+    if (input("1")):
+        print("du valde att skapa ett konto")
+    elif (input("2")):
+        print("2. administrera")
+    elif (input("3"))
+        print("3 avsluta")
+        print("test")
